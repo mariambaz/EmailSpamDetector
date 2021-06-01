2e945a89945eee3885d9ce0f4cb3c6c0ca308908
Mariam El Baz <60982075+mariambaz@users.noreply.github.com>
2021-06-01 12:25:28 +0200

    Added punctuation removal

--- spamHamDetector.py
+++ spamHamDetector.py
@@ -3,6 +3,16 @@ import pandas as pd
 
 ##MAIN##
 if __name__ == "__main__":
-  print("HELLO")
+  #Reading data from
   dataFrame = pd.read_csv (r'./spam_ham_dataset.csv')
-  print (dataFrame)
+  #Preprocessing
+  punc = '''!()-[]\{\};:'"\,<>./?@=#$%^&*_~'''
+  afterPunc = []
+  for b in dataFrame['Body']:
+    for char in b:
+      if (char in punc):
+        b = b.replace(char, "")
+    
+    afterPunc.append(b)
+  dataFrame['Body'] = afterPunc
+  print(dataFrame['Body'])
