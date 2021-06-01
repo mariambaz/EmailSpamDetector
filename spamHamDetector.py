9f3db0da784d9d130ded4653c20622fe546f2ecd
Mariam El Baz <60982075+mariambaz@users.noreply.github.com>
2021-06-01 12:35:41 +0200

    Added stop word detection and removal

--- spamHamDetector.py
+++ spamHamDetector.py
@@ -5,14 +5,22 @@ import pandas as pd
 if __name__ == "__main__":
   #Reading data from
   dataFrame = pd.read_csv (r'./spam_ham_dataset.csv')
+
   #Preprocessing
+  f = open('english-stopwords.txt','r')
+  stopWords = [word.strip() for word in f.readlines()]
+  f.close()
   punc = '''!()-[]\{\};:'"\,<>./?@=#$%^&*_~'''
-  afterPunc = []
+  preprocessed = []
   for b in dataFrame['Body']:
     for char in b:
       if (char in punc):
         b = b.replace(char, "")
-    
-    afterPunc.append(b)
-  dataFrame['Body'] = afterPunc
+    puncRemoved = ""
+    for word in b.split():
+      if (word in stopWords):
+        continue
+      puncRemoved += word + " "
+    preprocessed.append(puncRemoved)
+  dataFrame['Body'] = preprocessed
   print(dataFrame['Body'])
